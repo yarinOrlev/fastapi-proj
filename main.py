44b67bb031aebd182ce03f487d42c270951446fd
Yarin Orlev <yarin9191@gmail.com>
2025-04-13 11:52:51 +0300

    some learned routes

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data':{'name':'yarin'}}
+@app.get('/blog')
+def index(limit = 10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data':f'{limit} published blogs from db'}
+    else:
+        return {'data':f'{limit} blogs from db'}
+    
 
-@app.get('/about')
-def about():
-    return {'data':'about page'}
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':'all unpublished blogs'}
+
+@app.get('/blog/{id}')
+def show(id: int):
+    # fetch blog with id = id
+    return {'data': id}
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    #fetch comments blog with id = id
+    return {'data':{'1','2'}}
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    return {'data': f"blog is created with the title {request.title}"}
